96094a48eea529079e177868c719ef2b1d870602
Miguel Menjivar <menjivarmiguel221@gmail.com>
2021-04-26 21:46:26 -0700

    saved todo items to an array, now will attempt to push them into another component and view them there

--- react-todo/src/Form.js
+++ react-todo/src/Form.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todoItems: [],
+            value: ''
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    // on form submit
+    handleSubmit(event) {
+        event.preventDefault();
+
+        let newItem = this.state.value;
+
+        this.setState({
+            todoItems: [...this.state.todoItems, newItem],
+            value: ''
+        })
+    }
+
+    handleChange(event) {
+        this.setState({
+            value: event.target.value
+        })
+    }
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <input 
                         type="text" 
-                        name="name" 
+                        name="item" 
                         placeholder="Write todo here"
+                        value={this.state.value}
+                        onChange={this.handleChange}
                     />
+                    <button type="submit">Submit</button>
                 </form>
             </div>
         )
