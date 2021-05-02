f6400ffa9596ea74f0540f5b00fe488771023af3
Miguel Menjivar <menjivarmiguel221@gmail.com>
2021-05-01 23:12:45 -0700

    managed to get the remove functionality to work, now will add some css to style it and make it look nicer.

--- react-todo/src/components/View.js
+++ react-todo/src/components/View.js
@@ -4,12 +4,18 @@ const View = (props) => {
         const todoStyle= {
             listStyleType: "none",
         }
+
+        // for deleting item, and passing
+        // data upward
+        const onClickHandler = (id) => {
+            props.onRemoveItem(id);
+        }
         return (
             <div>
                 <h3>View Todos</h3>
                 <ul>
                     {props.listTodos.map(todoList => 
-                        <li key={todoList.id} style={todoStyle}>{todoList.task}</li>
+                        <li key={todoList.id} style={todoStyle}>{todoList.task}<button onClick={() => onClickHandler(todoList.id)}>Remove</button></li>
                     )}
                 </ul>
             </div>
